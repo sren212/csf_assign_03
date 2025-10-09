ded60b9b6cc99bfe9b13fcd0c03cbed430915ac7
sren212 <sren21@jh.edu>
2025-10-09 19:27:38 -0400

    begin initial file reading stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main( int argc, char **argv ) {
   // TODO: implement
 
+  ifstream f("README.txt"); // open file
+
+  if (!f.is_open()) { // check for errors opening the file
+    cerr << "ERROR OPENING TRACE FILE";
+    return 1;
+  }
+
+  string line; // hold 1 line of the file at a time
+  while (getline(f, line)) {
+
+    stringstream line_stream(line);
+    string field;
+
+    // first field is load/store
+    getline(line_stream, field, ' '); 
+    string op_string = field;
+    
+    // second field is memory address
+    getline(line_stream, field, ' ');
+    string address_string = field;
+
+    // ignore third field.
+
+    // do operations
+  }
+
+  f.close();
   return 0;
 }
