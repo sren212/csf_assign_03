6962bf1a0a330cbc8582b12e6c928e874ab367be
Lena Shea <lenashea03@gmail.com>
2025-10-09 20:29:16 -0400

    added all command line argument-related code

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main( int argc, char **argv ) {
-  if(argc != 9){
+  if(argc != 7){
     cerr << "INCORRECT ARGUMENT INPUT" << std::endl;
     return 1;
   }
@@ -18,8 +18,8 @@ int main( int argc, char **argv ) {
     if(!isPowerOfTwo(vars.setNum)){
       throw std::invalid_argument("Number of sets must be a positive power-of-2");
     }
-  }catch(const std::invalid_argument&){
-    cerr << "Invalid argument" << std::endl;
+  }catch(const std::invalid_argument& e){
+    cerr << "Invalid argument: " << e.what() << std::endl;
     return 1;
   }
 
@@ -30,8 +30,8 @@ int main( int argc, char **argv ) {
     if(!isPowerOfTwo(vars.blockNum)){
       throw std::invalid_argument("Number of blocks must be a positive power-of-2");
     }
-  }catch(const std::invalid_argument&){
-    cerr << "Invalid argument" << std::endl;
+  }catch(const std::invalid_argument& e){
+    cerr << "Invalid argument: " << e.what() << std::endl;
     return 1;
   }
 
@@ -42,8 +42,50 @@ int main( int argc, char **argv ) {
     if(!isPowerOfTwo(vars.bytesPerBlock)){
       throw std::invalid_argument("Bytes per block must be a positive power-of-2");
     }
-  }catch(const std::invalid_argument&){
-    cerr << "Invalid argument" << std::endl;
+  }catch(const std::invalid_argument& e){
+    cerr << "Invalid argument: " << e.what() << std::endl;
+    return 1;
+  }
+
+  //write-allocate
+  try{
+    if(strcmp(argv[4], "write-allocate")){
+      vars.writeAllocate = true;
+    }else if(strcmp(argv[4], "no-write-allocate")){
+      vars.writeAllocate = false;
+    }else{
+      throw std::invalid_argument("must enter write-allocate/no-write-allocate for 4th argument");
+    }
+  }catch(const std::invalid_argument& e){
+    cerr << "Invalid argument: " << e.what() << std::endl;
+    return 1;
+  }
+
+  //write-through / write-back
+  try{
+    if(strcmp(argv[5], "write-through")){
+      vars.writeThrough = true;
+    }else if(strcmp(argv[5], "write-back")){
+      vars.writeThrough = false;
+    }else{
+      throw std::invalid_argument("must enter write-through/no-write-back for 5th argument");
+    }
+  }catch(const std::invalid_argument& e){
+    cerr << "Invalid argument: " << e.what() << std::endl;
+    return 1;
+  }
+
+  //lru / fifo
+  try{
+    if(strcmp(argv[6], "lru")){
+      vars.eviction = true;
+    }else if(strcmp(argv[6], "fifo")){
+      vars.eviction = false;
+    }else{
+      throw std::invalid_argument("must enter lru / fifo for 6th argument");
+    }
+  }catch(const std::invalid_argument& e){
+    cerr << "Invalid argument: " << e.what() << std::endl;
     return 1;
   }
 
@@ -57,8 +99,10 @@ int main( int argc, char **argv ) {
   int storeMisses = 0;
   int cycles = 0;
 
+  /*
+
   // open file
-  ifstream f(argv[8]);
+  ifstream f(argv[7]);
 
   // check for errors opening the file
   if (!f.is_open()) {
@@ -92,9 +136,17 @@ int main( int argc, char **argv ) {
   }
 
   f.close();
+  */
+
+  std::string line;
+  while(std::getline(std::cin, line)){
+    //do operations on "line" for each line of input
+  }
 
   // output summary info
   printSummary(loads, stores, loadHits, loadMisses, storeHits, storeMisses, cycles);
+
+
 
   return 0;
 }
