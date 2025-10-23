bc20f517cc6de6321610eecc667cda9b880f5c24
Lena Shea <lenashea03@gmail.com>
2025-10-22 21:00:23 -0400

    add dirty bit stuff lol

--- cache_fns.cpp
+++ cache_fns.cpp
@@ -220,7 +220,7 @@ bool updateSlot(Cache *cache, uint32_t tag, uint32_t index, bool lru) {
             // update slot
             (*curr).tag = tag;
             (*curr).valid = true;
-            (*curr).dirty = false;
+            (*curr).dirty = true;
             (*curr).load_ts = 0;
             (*curr).access_ts = 0;
             return evict_dirty;
